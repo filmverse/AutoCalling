9c6f5aa137d14271a7e538f97d934ea7d54320f7
VIKAS SHARMA <vkassharma19492@gmail.com>
2023-06-30 19:19:17 +0530

    setting up project

--- AutoCalling/App.js
+++ AutoCalling/App.js
@@ -2,32 +2,18 @@ import {SafeAreaView, StyleSheet, Text, View, Button, Alert } from 'react-native
 
 const Separator = () => <View style={styles.separator} />
 
-export default function App() {
+const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.title}>Calling Buttons Automatially!</Text>
-        <Separator />
         <Button title='Press Button 1' onPress={() => Alert.alert('You press the button 1')} />
+        <Separator />
         <Button title='Press Button 2' onPress={() => Alert.alert('You press the button 2')} />
         <Separator />
-        <Button title='Press Button 3' onPress={() => Alert.alert('You press the button 3')} />
-        <Separator />
-        <Button title='Press Button 4' onPress={() => Alert.alert('You press the button 4')} />
-        <Separator />
         <View style={styles.fixToText}>
-          <Button title='Press Button 5' onPress={() => Alert.alert('You press the button 5')} />
-          <Button title='Press Button 6' onPress={() => Alert.alert('You press the button 6')} />
-        </View>
-        <Separator />
-        <View style={styles.fixToText}>
-          <Button title='Press Button 7' onPress={() => Alert.alert('You press the button 7')} />
-          <Button title='Press Button 8' onPress={() => Alert.alert('You press the button 8')} />
-        </View>
-        <Separator />
-        <View style={styles.fixToText}>
-          <Button title='Press Button 9' onPress={() => Alert.alert('You press the button 9')} />
-          <Button title='Press Button 10' onPress={() => Alert.alert('You press the button 10')} />
+          <Button title='Press Button 3' onPress={() => Alert.alert('You press the button 3')} />
+          <Button title='Press Button 4' onPress={() => Alert.alert('You press the button 4')} />
         </View>
       </View>
     </SafeAreaView>
@@ -54,3 +40,5 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
+
+export default App;
