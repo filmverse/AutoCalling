dca6953983a7d86ac68484d911c16c177c25a7fa
VIKAS SHARMA <vkassharma19492@gmail.com>
2023-07-01 10:16:44 +0530

    calling buttons without pressing them

--- AutoCalling/App.js
+++ AutoCalling/App.js
@@ -1,28 +1,47 @@
-import {SafeAreaView, StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { useState, useEffect } from 'react';
 
 const Separator = () => <View style={styles.separator} />
 
 const App = () => {
 
-  const [ button1Pressed, setButton1Pressed ] = useState(0)
-  const [ button2Pressed, setButton2Pressed ] = useState(0)
-  const [ button3Pressed, setButton3Pressed ] = useState(0)
-  const [ button4Pressed, setButton4Pressed ] = useState(0)
+  useEffect(() => {
+    // Call the buttons automatically after a delay
+    setTimeout(() => {
+      handleButtonPress1();
+      handleButtonPress2();
+      handleButtonPress3();
+      handleButtonPress4();
+    }, 2000);
+  }, []);
 
-  const handleButtonPressed = (handleObject) => (event) => {}
+  const handleButtonPress1 = () => {
+    console.log('You pressed Button 1');
+  };
+
+  const handleButtonPress2 = () => {
+    console.log('You pressed Button 2');
+  };
+
+  const handleButtonPress3 = () => {
+    console.log('You pressed Button 3');
+  };
+
+  const handleButtonPress4 = () => {
+    console.log('You pressed Button 4');
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text style={styles.title}>Calling Buttons Automatially!</Text>
-        <Button title='Press Button 1' onPress={() => Alert.alert('You press the button 1')} />
+        <Text style={styles.title}>Calling Buttons Automatically!</Text>
+        <Button title="Press Button 1" onPress={handleButtonPress1} />
         <Separator />
-        <Button title='Press Button 2' onPress={() => Alert.alert('You press the button 2')} />
+        <Button title="Press Button 2" onPress={handleButtonPress2} />
         <Separator />
         <View style={styles.fixToText}>
-          <Button title='Press Button 3' onPress={() => Alert.alert('You press the button 3')} />
-          <Button title='Press Button 4' onPress={() => Alert.alert('You press the button 4')} />
+          <Button title="Press Button 3" onPress={handleButtonPress3} />
+          <Button title="Press Button 4" onPress={handleButtonPress4} />
         </View>
       </View>
     </SafeAreaView>
