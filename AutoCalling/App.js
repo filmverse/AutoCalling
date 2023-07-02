7682d8ce63dca8d65c5af14bf17a4e21b5fabe21
VIKAS SHARMA <vkassharma19492@gmail.com>
2023-07-03 02:32:14 +0530

    autocalling funcations

--- AutoCalling/App.js
+++ AutoCalling/App.js
@@ -1,52 +1,85 @@
+import React, { useEffect, useRef } from 'react';
 import { SafeAreaView, StyleSheet, Text, View, Button, Alert } from 'react-native';
-import { useState, useEffect } from 'react';
 
-const Separator = () => <View style={styles.separator} />
+const Separator = () => <View style={styles.separator} />;
 
 const App = () => {
+  const buttonRef1 = useRef(null);
+  const buttonRef2 = useRef(null);
+  const buttonRef3 = useRef(null);
+  const buttonRef4 = useRef(null);
+
+  // useEffect(() => {
+  //   // Call the buttons automatically after a delay
+  //   setTimeout(() => {
+  //     handleButtonPress1();
+  //     handleButtonPress2();
+  //     handleButtonPress3();
+  //     handleButtonPress4();
+  //   }, 2000);
+  // }, []);
 
   useEffect(() => {
-    // Call the buttons automatically after a delay
-    setTimeout(() => {
-      handleButtonPress1();
-      handleButtonPress2();
-      handleButtonPress3();
-      handleButtonPress4();
-    }, 2000);
+    autoCallButtons();
   }, []);
 
   const handleButtonPress1 = () => {
+    Alert.alert('You pressed Button 1');
     console.log('You pressed Button 1');
   };
 
   const handleButtonPress2 = () => {
+    Alert.alert('You pressed Button 2');
     console.log('You pressed Button 2');
   };
 
   const handleButtonPress3 = () => {
+    Alert.alert('You pressed Button 3');
     console.log('You pressed Button 3');
   };
 
   const handleButtonPress4 = () => {
+    Alert.alert('You pressed Button 4');
     console.log('You pressed Button 4');
+  };
+
+  const autoCallButtons = () => {
+    // Simulate button press after a delay
+    setTimeout(() => {
+      buttonRef1.current && buttonRef1.current.props.onPress();
+    }, 1000);
+
+    setTimeout(() => {
+      buttonRef2.current && buttonRef2.current.props.onPress();
+    }, 2000);
+
+    setTimeout(() => {
+      buttonRef3.current && buttonRef3.current.props.onPress();
+    }, 4000);
+
+    setTimeout(() => {
+      buttonRef4.current && buttonRef4.current.props.onPress();
+    }, 8000);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.title}>Calling Buttons Automatically!</Text>
-        <Button title="Press Button 1" onPress={handleButtonPress1} />
+        <Button ref={buttonRef1} title="Press Button 1" onPress={handleButtonPress1} />
         <Separator />
-        <Button title="Press Button 2" onPress={handleButtonPress2} />
+        <Button ref={buttonRef2} title="Press Button 2" onPress={handleButtonPress2} />
         <Separator />
         <View style={styles.fixToText}>
-          <Button title="Press Button 3" onPress={handleButtonPress3} />
-          <Button title="Press Button 4" onPress={handleButtonPress4} />
+          <Button ref={buttonRef3} title="Press Button 3" onPress={handleButtonPress3} />
+          <Button ref={buttonRef4} title="Press Button 4" onPress={handleButtonPress4} />
         </View>
+        <Separator />
+        <Button title="Auto Call Buttons" onPress={autoCallButtons} />
       </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
